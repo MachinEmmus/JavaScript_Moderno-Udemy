8a8624ab0b216c1f0c761fb5000fb8abb5b05757
Emmus Monsalve <emonsalvep38@gmail.com>
2020-05-11 16:36:57 -0500

    Add a new knowlegde about JavaScript

--- Fundaments/app.js
+++ Fundaments/app.js
@@ -1,5 +1,14 @@
+// hello world
 
 let name = prompt('Cual es tu nombre?')
 let age = prompt('Cual es tu edad?')
 
 document.getElementById('app').innerHTML = `Hello World ${name} with ${age} yars old`;
+
+// var let const
+// var redeclarar un variable
+var car = "Mercedez";
+// let para valores que se pueden reescribir
+let hi = "Syria";
+// Const un valor que no puede cambiar
+const value = "Valor constante" 
