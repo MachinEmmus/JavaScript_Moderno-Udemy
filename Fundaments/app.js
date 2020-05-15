d6efc4f8cfa4944df23621bd4f52942a4eefbc39
Emmus Monsalve <emonsalvep38@gmail.com>
2020-05-15 17:04:23 -0500

    Add new knowlegde about JavaScipt

--- Fundaments/app.js
+++ Fundaments/app.js
@@ -12,3 +12,29 @@ var car = "Mercedez";
 let hi = "Syria";
 // Const un valor que no puede cambiar
 const value = "Valor constante" 
+// Templates
+const fov = 103,
+    SPubg = 35,
+    SValorant = 0.419,
+    Mdpi = 800;
+
+html = `<ul>
+            <li>Estudiar JavaScript, Apis</li>
+            <li>App en FLutter</li>
+            <li> STYLE </li>
+            <li> Aim training </li>
+        </ul>
+        <p>Mis Sensibilidades en fps son: </p>
+        <ul>
+            <li> Valorant: ${SValorant} </li>
+            <li> Pubg: ${SPubg} </li>
+            <li> Mi eDPI en Pubg: ${edpi(SPubg, Mdpi)}</li>
+            <li> Mi eDPI en Valorant: ${edpi(SValorant, Mdpi)}</li>
+        </ul>
+        `;
+ 
+function edpi(gameS, MSens){
+    return gameS * MSens;
+}        
+
+document.getElementById('app').innerHTML = html;
